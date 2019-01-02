2a5bc6fe459d42bdc422a20f3a50c19b2daa911e
Joshua Lee <joshslee93@gmail.com>
2019-01-02 14:52:02 -0800

    finished component

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    env:{
-      test: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: [
-          'transform-es2015-modules-commonjs',
-          '@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-export-namespace-from',
-          '@babel/plugin-proposal-throw-expressions',
-        ],
-        only: ["./**/*.js", "node_modules/jest-runtime"]
-      }
-    }
+  "presets": [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        "loose": true,
+        "modules": false
+      },
+    ],
+  ],
+  "plugins": [
+    "transform-es2015-modules-commonjs"
+  ]
 };
